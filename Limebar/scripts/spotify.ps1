1183d22cee23b0ef20449e1ac0a80172b3da4409
JaeHeyK <jaeheyk@gmail.com>
2022-01-23 04:52:56 +0900

    Remove infinite loop and delay in spotify ps script

--- Limebar/scripts/spotify.ps1
+++ Limebar/scripts/spotify.ps1
@@ -1,12 +1,9 @@
 ﻿# Original code by https://github.com/Trezub/playing-now-spotify
-while ($true) {
-  $processes = Get-Process spotify
-  $title = (Out-String -InputObject $processes.mainWindowTitle) -replace "`n","" -replace "`r",""
-  $song = $null
-  if (-Not $title.Contains('Spotify')) {
-      $song = $title
-  }
-  cls
-  Write-Output "$song"
-  Start-Sleep -Seconds 1
+$processes = Get-Process spotify
+$title = (Out-String -InputObject $processes.mainWindowTitle) -replace "`n","" -replace "`r",""
+$song = $null
+if (-Not $title.Contains('Spotify')) {
+  $song = $title
 }
+cls
+Write-Output "$song"
