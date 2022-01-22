3dfcd802096fda9b35f382f5f099288a588e9937
JaeHeyK <jaeheyk@gmail.com>
2022-01-23 04:45:48 +0900

    Add spotify song title ps script

---
+++ Limebar/scripts/spotify.ps1
@@ -0,0 +1,12 @@
+﻿# Original code by https://github.com/Trezub/playing-now-spotify
+while ($true) {
+  $processes = Get-Process spotify
+  $title = (Out-String -InputObject $processes.mainWindowTitle) -replace "`n","" -replace "`r",""
+  $song = $null
+  if (-Not $title.Contains('Spotify')) {
+      $song = $title
+  }
+  cls
+  Write-Output "$song"
+  Start-Sleep -Seconds 1
+}
