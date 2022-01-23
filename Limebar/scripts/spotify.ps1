0d8103cfa11987022d5a58cb964ee57f7fd9320e
JaeHeyK <jaeheyk@gmail.com>
2022-01-23 23:57:59 +0900

    Edit spotify script to contain pause, unavailable status

--- Limebar/scripts/spotify.ps1
+++ Limebar/scripts/spotify.ps1
@@ -1,9 +1,16 @@
 ﻿# Original code by https://github.com/Trezub/playing-now-spotify
-$processes = Get-Process spotify
-$title = (Out-String -InputObject $processes.mainWindowTitle) -replace "`n","" -replace "`r",""
-$song = $null
-if (-Not $title.Contains('Spotify')) {
-  $song = $title
+$processes = Get-Process spotify -ErrorAction SilentlyContinue -ErrorVariable ProcessError
+if($ProcessError) {
+    Write-Output "…"
+} else {
+    $title = (Out-String -InputObject $processes.mainWindowTitle) -replace "`n","" -replace "`r",""
+    $song = $null
+        if (-Not $title.Contains('Spotify')) {
+            $song = $title
+            $song | Out-File -encoding ascii .\currentSong.txt -NoNewLine
+            Write-Output "♬ $song"
+        } else {
+            $song = Get-Content .\currentSong.txt -Raw
+            Write-Output "⏸ $song"
+        }
 }
-
-Write-Output "$song"
